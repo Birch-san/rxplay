773ea9cf3d93d10f440e306ba0a855bb64906056
Alex Birch <Birch-san@users.noreply.github.com>
2021-03-13 20:07:10 +0000

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['plugin:@typescript-eslint/recommended'],
+  extends: 'standard-with-typescript',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'eol-last': 'off'
+  }
 }
