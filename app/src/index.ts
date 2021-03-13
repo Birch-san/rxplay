fd8c2e5b7d33208b3a73bf9bfb0c9602e61ca119
Alex Birch <Birch-san@users.noreply.github.com>
2021-03-13 22:53:01 +0000

    simplify module resolution

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,4 +1,7 @@
 import liveServer from 'live-server'
+import { dirname } from 'path'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 
 liveServer.start({
   open: false,
@@ -6,6 +9,6 @@ liveServer.start({
   port: 3000,
   root: 'public',
   mount: [
-    ['/web_modules/lib', '../node_modules/@birchlabs-rxplay/lib/dist']
+    ['/web_modules/lib', dirname(require.resolve('@birchlabs-rxplay/lib'))]
   ]
 })
