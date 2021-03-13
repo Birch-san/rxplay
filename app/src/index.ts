61ae275be0ac9a3c33afb7a22e06796578008198
Alex Birch <Birch-san@users.noreply.github.com>
2021-03-13 22:40:16 +0000

    app loads lib

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,24 @@
-console.log('hey')
+import liveServer, { LiveServerParams } from 'live-server'
+
+// declare module 'live-server' {
+//   interface LiveServerParams {
+//     watch: string[]
+//   }
+// }
+
+const libDist = '../node_modules/@birchlabs-rxplay/lib/dist'
+
+const liveServerParams: LiveServerParams = {
+  // Opens the local server on start.
+  open: false,
+  host: '127.0.0.1',
+  port: 3000,
+  // Uses `public` as the local server folder.
+  root: 'public',
+  // watch: [libDist, 'public'],
+  mount: [
+    ['/web_modules/lib', libDist]
+  ]
+}
+
+liveServer.start(liveServerParams)
